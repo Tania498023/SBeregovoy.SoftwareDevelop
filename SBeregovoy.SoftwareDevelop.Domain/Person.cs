18e62b2dba9db812a018a8b98d9d5fb3acd71c2a
Tania <perekhval@rambler.ru>
2022-07-26 14:05:55 +0300

    создала Классы

--- SBeregovoy.SoftwareDevelop.Domain/Person.cs
+++ SBeregovoy.SoftwareDevelop.Domain/Person.cs
@@ -8,5 +8,29 @@ namespace SBeregovoy.SoftwareDevelop.Domain
 {
     public class Person
     {
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public int Hoursworked { get; set; }
+
+        public Person(string name, decimal salary, int hoursworked)
+        {
+            Name = name;
+            Salary = salary;
+            Hoursworked = hoursworked;
+        }
+        public class Manager 
+
+        {
+            decimal oklad = 200000;
+            decimal premia = 20000;
+            int hours = 158;
+            int fullhours = 160;
+
+            public void RaschetSalary(decimal oklad, int hours, decimal premia)
+            {
+                
+            }
+        }
     }
+
 }
