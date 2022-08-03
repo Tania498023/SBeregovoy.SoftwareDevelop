c8a5f220e26eec79e2b2405ea5d694a98baf2c97
Tania <perekhval@rambler.ru>
2022-08-03 16:07:09 +0300

    исправлен путь к файлу

--- Persistence/FileRepository.cs
+++ Persistence/FileRepository.cs
@@ -14,7 +14,7 @@ namespace SBeregovoy.SoftwareDevelop.Persistence
         public void FillFileUser(List<User> users)
         {
 
-            string userpath = @"C: \Users\Tanya\source\repos\SBeregovoy.SoftwareDevelop\SoftwareDevelopConsole\Data\User.csv";
+            string userpath = @"C:\Users\Tanya\source\repos\SBeregovoy.SoftwareDevelop\SoftwareDevelopConsole\Data\User.csv";
             foreach (User user in users)//перебираем коллекцию и выбираем из нее элементы
             {
                 string userstr = user.Name + "," + user.UserRole + Environment.NewLine;//создаем строку с разделительными символами и переносом строки
@@ -26,7 +26,7 @@ namespace SBeregovoy.SoftwareDevelop.Persistence
         public void FillFileEmployee(List<TimeRecord> timeRecords)
         {
 
-            string employeepath = @"C: \Users\Tanya\source\repos\SBeregovoy.SoftwareDevelop\SoftwareDevelopConsole\Data\Employee.csv";
+            string employeepath = @"C:\Users\Tanya\source\repos\SBeregovoy.SoftwareDevelop\SoftwareDevelopConsole\Data\Employee.csv";
             foreach (TimeRecord emloyees in timeRecords)//перебираем коллекцию и выбираем из нее элементы
             {
                 string employeestr = emloyees.Date + "," + emloyees.Name + emloyees.Hours + emloyees.Message + Environment.NewLine;//создаем строку с разделительными символами и переносом строки
@@ -38,7 +38,7 @@ namespace SBeregovoy.SoftwareDevelop.Persistence
         public void FillFileFrilanser(List<TimeRecord> timeRecords)
         {
 
-            string frilanserpath = @"C: \Users\Tanya\source\repos\SBeregovoy.SoftwareDevelop\SoftwareDevelopConsole\Data\Frilanser.csv";
+            string frilanserpath = @"C:\Users\Tanya\source\repos\SBeregovoy.SoftwareDevelop\SoftwareDevelopConsole\Data\Frilanser.csv";
             foreach (TimeRecord frilanser in timeRecords)//перебираем коллекцию и выбираем из нее элементы
             {
                 string frilanserstr = frilanser.Date + "," + frilanser.Name + frilanser.Hours + frilanser.Message + Environment.NewLine;//создаем строку с разделительными символами и переносом строки
@@ -50,7 +50,7 @@ namespace SBeregovoy.SoftwareDevelop.Persistence
         public void FillFileManager(List<TimeRecord> timeRecords)
         {
 
-            string managerpath = @"C: \Users\Tanya\source\repos\SBeregovoy.SoftwareDevelop\SoftwareDevelopConsole\Data\Manager.csv";
+            string managerpath = @"C:\Users\Tanya\source\repos\SBeregovoy.SoftwareDevelop\SoftwareDevelopConsole\Data\Manager.csv";
             foreach (TimeRecord manager in timeRecords)//перебираем коллекцию и выбираем из нее элементы
             {
                 string managerstr = manager.Date + "," + manager.Name + manager.Hours + manager.Message + Environment.NewLine;//создаем строку с разделительными символами и переносом строки
