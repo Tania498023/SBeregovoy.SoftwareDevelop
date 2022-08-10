19aaaf4f359e66cba8675df4917c1e7c5846a2a3
Tania <perekhval@rambler.ru>
2022-08-10 09:18:39 +0300

    добавлен ввод роли и созданы фейковые меню

--- SBeregovoy.SoftwareDevelop.Domain/Persons/User.cs
+++ SBeregovoy.SoftwareDevelop.Domain/Persons/User.cs
@@ -17,4 +17,6 @@ namespace SBeregovoy.SoftwareDevelop.Domain
             UserRole = userRole;
         }
     }
+
+
 }
--- SoftwareDevelopConsole/Program.cs
+++ SoftwareDevelopConsole/Program.cs
@@ -10,6 +10,8 @@ namespace SBeregovoy.SoftwareDevelop.SoftwareDevelopConsole
 {
     internal class Program
     {
+        private static UserRole enteruser;
+
         static void Main(string[] args)
 
         {
@@ -17,16 +19,84 @@ namespace SBeregovoy.SoftwareDevelop.SoftwareDevelopConsole
 
             var fill = new FileRepository();//создаем экземпляры для возможности вызова метода FillFileUser
             var userreturn = new MemoryRepository();//создаем экземпляры для возможности вызова метода Users
-            fill.FillFileUser(userreturn.Users(),false);//вызываем методы FillFileUser и Users
+            fill.FillFileUser(userreturn.Users(), false);//вызываем методы FillFileUser и Users
 
-            
+
             var genericreturn = new MemoryRepository();//создаем экземпляры для возможности вызова метода Employees
             fill.FillFileGeneric(genericreturn.Generic(), userRole, false);//вызываем методы FillFileEmployee и Employees
 
             var text = fill.ReadFileUser();
+            
+            ControlRole();
+        }
+        public static void ControlRole()//контроль вводимой роли при входе в программу
+        {
+            
+            do
+            {
+                Console.WriteLine("Введите роль \n Введите 0, если вы менеджер \n Введите 1, если вы сотрудник \n Введите 2, если вы фрилансер");
+                int controleRole = Convert.ToInt32(Console.ReadLine());
 
+                if (controleRole == (int)UserRole.Manager)
+                {
+                    enteruser = UserRole.Manager;
+                    break;
+                }
+                
+                else if (controleRole == (int)UserRole.Employee)
+                {
+                    enteruser = UserRole.Employee;
+                    break;
+                }
+                else if (controleRole == (int)UserRole.Frelanser)
+                {
+                    enteruser = UserRole.Frelanser;
+                    break;
+                }
+                else
+                    Console.WriteLine("Вы ввели несуществующую роль");
+                }
 
+            while (enteruser >= UserRole.Manager && enteruser <= UserRole.Frelanser);
+            DisplayMenu(enteruser);
+        }
+
+        private static void DisplayMenu(UserRole userRole)
+        {
+            do
+            {
+                //Console.WriteLine(" ");
+                //int controleRole = Convert.ToInt32(Console.ReadLine());
+
+                if (userRole == UserRole.Manager)
+                {
+                    Console.WriteLine("Меню Руководитель");
+                    Console.WriteLine("1. добавить сотрудника");
+                    Console.WriteLine("2. добавить время сотруднику");
+                    Console.WriteLine("3. посмотреть отчет по всем сотрудникам (возможность выбрать период)");
+                    Console.WriteLine("4. посмотреть часы работы сотрудника");
+                    break;
+                }
+                if (userRole == UserRole.Employee)
+                {
+                    Console.WriteLine("Меню Сотрудник");
+                    Console.WriteLine("1. ввести часы");
+                    Console.WriteLine("2. просмотреть часы");
+                    break;
+                }
+                if (userRole == UserRole.Frelanser)
+                {
+                    Console.WriteLine("Меню Фрилансер");
+                    Console.WriteLine("1. ввести часы");
+                    Console.WriteLine("2. просмотреть часы");
+                    break;
+                }
+
+            }
+            while (true);
+            
         }
     }
 }
+
 
