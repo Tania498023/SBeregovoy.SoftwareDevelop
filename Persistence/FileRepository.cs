9daca1377b7ceaf1c08f35d0154128aa727563ea
Tania <perekhval@rambler.ru>
2022-08-30 13:35:52 +0300

    в класс FileRepository добавлены <summary> комментарии к методам

--- Persistence/FileRepository.cs
+++ Persistence/FileRepository.cs
@@ -10,6 +10,11 @@ namespace SBeregovoy.SoftwareDevelop.Persistence
 {
     public class FileRepository
     {
+        /// <summary>
+        /// Записываем коллекцию в файл user.csv 
+        /// </summary>
+        /// <param name="users"></param>
+        /// <param name="userneedwrite"></param>
         public void FillFileUser(List<User> users, bool userneedwrite)
         {
             string userpath = @".\Data\User.csv";
@@ -27,12 +32,42 @@ namespace SBeregovoy.SoftwareDevelop.Persistence
                 File.AppendAllText(userpath, userstr);//записываем указанную строку 
             }
         }
-        
 
-        //создаем общий метод для сотрудников
+
+        /// <summary>
+        /// Записываем коллекцию TimeRecord в файл согласно роли
+        /// </summary>
+        /// <param name="timeRecords"></param>
+        /// <param name="roles"></param>
+        /// <param name="genericneedwrite"></param>
         public void FillFileGeneric(List<TimeRecord> timeRecords, int roles, bool genericneedwrite)
         {
-          
+            string newpath = ConvertRoleToPath(roles);
+
+            if (!File.Exists(newpath))
+                return;
+            System.IO.FileInfo file = new System.IO.FileInfo(newpath);
+            long size = file.Length;
+
+            if (!genericneedwrite && size > 0)
+                return;
+
+            foreach (TimeRecord userrole in timeRecords)//перебираем коллекцию и выбираем из нее элементы
+            {
+                //создаем строку с разделительными символами и переносом строки
+                string genericstr = userrole.Date + "," + userrole.Name + "," + userrole.Hours + "," + userrole.Message + Environment.NewLine;
+
+                File.AppendAllText(newpath, genericstr);//записываем указанную строку 
+            }
+        }
+
+        /// <summary>
+        /// Конвертируем тип int(роль) в string(путь к файлу) 
+        /// </summary>
+        /// <param name="roles"></param>
+        /// <returns></returns>
+        private static string ConvertRoleToPath(int roles)
+        {
             string newpath = "";
 
             if (roles == (int)UserRole.Manager)
@@ -48,21 +83,12 @@ namespace SBeregovoy.SoftwareDevelop.Persistence
                 newpath = @".\Data\Frilanser.csv";
             }
 
-            if (!File.Exists(newpath))
-                return;
-            System.IO.FileInfo file = new System.IO.FileInfo(newpath);
-            long size = file.Length;
-
-            if (!genericneedwrite && size > 0)
-                return;
-
-            foreach (TimeRecord userrole in timeRecords)//перебираем коллекцию и выбираем из нее элементы
-            {
-                string genericstr = userrole.Date + "," + userrole.Name + "," + userrole.Hours + "," + userrole.Message + Environment.NewLine;//создаем строку с разделительными символами и переносом строки
-                File.AppendAllText(newpath, genericstr);//записываем указанную строку 
-            }
-            
+            return newpath;
         }
+        /// <summary>
+        /// Считывает все строки файла User.csv и закрывает файл
+        /// </summary>
+        /// <returns></returns>
         public List<User> ReadFileUser()
         {
             var defaultuser = new User("defaultuser", UserRole.Manager);
@@ -94,31 +120,21 @@ namespace SBeregovoy.SoftwareDevelop.Persistence
             }
             return users;
         }
-
+        /// <summary>
+        /// Считывает все строки файла согласно роли
+        /// </summary>
+        /// <param name="roles"></param>
+        /// <returns></returns>
         public List<TimeRecord> ReadFileGeneric(int roles)
         {
-            string newpath = "";
+            string newpath = ConvertRoleToPath(roles);
 
-            if (roles == (int)UserRole.Manager)
-            {
-                newpath = @".\Data\Manager.csv";
-            }
-            if (roles == (int)UserRole.Employee)
-            {
-                newpath = @".\Data\Employee.csv";
-            }
-            else if (roles == (int)UserRole.Frelanser)
-            {
-                newpath = @".\Data\Frilanser.csv";
-            }
-            
             if (!File.Exists(newpath))
                 return null;
             List<TimeRecord> generic = new List<TimeRecord>();
 
             var strings = File.ReadAllLines(newpath);//получили массив строк
            
-
             foreach (var stroka in strings)
             {
                 if (string.IsNullOrEmpty(stroka))
@@ -130,6 +146,11 @@ namespace SBeregovoy.SoftwareDevelop.Persistence
             }
             return generic;
         }
+        /// <summary>
+        /// Получаем имя пользователя и возвращаем коллекцию User
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
         public User UserGet(string name)
         {
             foreach (var record in ReadFileUser())
@@ -139,25 +160,40 @@ namespace SBeregovoy.SoftwareDevelop.Persistence
             }
             return null;
         }
-
-
-        public List<TimeRecord> ReportGet(UserRole userRole, DateTime? from = null, DateTime? to = null)
+        /// <summary>
+        /// Получаем отчет по сотрудникам с учетом роли 
+        /// </summary>
+        /// <param name="userRole"></param>
+        /// <param name="startdate"></param>
+        /// <param name="enddate"></param>
+        /// <returns></returns>
+        public List<TimeRecord> ReportGet(UserRole userRole, DateTime? startdate = null, DateTime? enddate = null)
         {
             var records = ReadFileGeneric((int)userRole);
 
-
-        
-            if (from == null)
+            if (startdate == null)
             {
-                from = DateTime.Now.Date.AddYears(-100);
+                startdate = DateTime.Now.Date.AddYears(-100);
             }
-            if (to == null)
+            if (enddate == null)
             {
-                to = DateTime.Now.Date;
+                enddate = DateTime.Now.Date;
             }
 
-            return records.Where(x => from.Value <= x.Date && x.Date <= to).ToList();
+            //берем каждую запись из коллекции records и сравниваем ее с переменной, которая передана в метод.
+            //если условие выполняется, то кладем элемент в ToList(). Where- аналог foreach с if/else. x- виртуальная переменная
+            // коллекции records- аналог item.Условие в данном случае: если x.Date меньше или равно конечной даты enddate(максимальная)
+            // и x.Date больше или равно начальной даты startdate(минимальная)
+            return records.Where(x => startdate.Value <= x.Date && enddate >= x.Date).ToList();
         }
+        /// <summary>
+        /// Получаем отчет по конкретному сотруднику
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="userRole"></param>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
         public List<TimeRecord> ReportGetByUser(string userName, UserRole userRole, DateTime? from = null, DateTime? to = null)
         {
             return ReportGet(userRole, from, to).Where(x => x.Name == userName).ToList();
